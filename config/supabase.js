fefe7c172307b4b3b4f3db6dca99b5a638d1a306
htpweb <aarboledaadv2@gmail.com>
2026-09-12 12:27:29 -0500

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL="https://hwfloywzqlgqieonuswl.supabase.co";
 const SUPABASE_KEY="sb_publishable_NYoHIme-48fZeXAjF6OZaQ_twFybYI1";
 
-const supabase=window.supabase.createClient(
+const supabaseClient=window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
