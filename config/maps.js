431b07b010df5a3bb6a7510eb87a4599dd4fefa4
htpweb <aarboledaadv2@gmail.com>
2026-09-19 22:02:27 -0500

    Código 90: activar Google Maps en HTPWEB

--- config/maps.js
+++ config/maps.js
@@ -1,3 +1,3 @@
 // Public browser key, restricted to the HTPWEB origin and Maps/Places APIs.
 // No key is provisioned and no paid API is enabled automatically.
-window.HTPWEB_MAPS = Object.freeze({googleKey: "", defaultCenter: [0.9592,-79.6539]});
+window.HTPWEB_MAPS = Object.freeze({googleKey: "AIzaSyBomOCyPe2TaHsbaS5tuuOJpXUW7wNHpvs", defaultCenter: [0.9592,-79.6539]});
