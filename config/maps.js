0ecc4e0c0119db2234ae1a036384381f01d0cd1e
htpweb <aarboledaadv2@gmail.com>
2026-09-19 22:08:47 -0500

    Update maps.js

--- config/maps.js
+++ config/maps.js
@@ -1,3 +1,3 @@
 // Public browser key, restricted to the HTPWEB origin and Maps/Places APIs.
 // No key is provisioned and no paid API is enabled automatically.
-window.HTPWEB_MAPS = Object.freeze({googleKey: "AIzaSyBomOCyPe2TaHsbaS5tuuOJpXUW7wNHpvs", defaultCenter: [0.9592,-79.6539]});
+window.HTPWEB_MAPS = Object.freeze({googleKey: "AIzaSyAQRCKPdWiczhl4B1uA5sPR_X_nGtkpUiU", defaultCenter: [0.9592,-79.6539]});
