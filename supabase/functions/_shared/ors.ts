d3f1d9c369519002a56624e9c77504e783d58dd9
htpweb <aarboledaadv2@gmail.com>
2026-09-25 10:29:18 -0500

    Add shared HeiGIT routing endpoints

---
+++ supabase/functions/_shared/ors.ts
@@ -0,0 +1,8 @@
+export const ORS_DIRECTIONS_DRIVING_URL =
+  "https://api.heigit.org/openrouteservice/v2/directions/driving-car";
+
+export const ORS_MATRIX_DRIVING_URL =
+  "https://api.heigit.org/openrouteservice/v2/matrix/driving-car";
+
+export const ORS_OPTIMIZATION_URL =
+  "https://api.heigit.org/vroom/v0";
