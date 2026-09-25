c43cd02f05eca2ed09087023429978cb466d3da0
htpweb <aarboledaadv2@gmail.com>
2026-09-25 12:23:49 -0500

    Add ORS GeoJSON directions endpoint

--- supabase/functions/_shared/ors.ts
+++ supabase/functions/_shared/ors.ts
@@ -1,5 +1,8 @@
 export const ORS_DIRECTIONS_DRIVING_URL =
   "https://api.heigit.org/openrouteservice/v2/directions/driving-car";
 
+export const ORS_DIRECTIONS_DRIVING_GEOJSON_URL =
+  ORS_DIRECTIONS_DRIVING_URL + "/geojson";
+
 export const ORS_OPTIMIZATION_URL =
   "https://api.heigit.org/vroom/v0";
