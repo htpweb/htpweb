eaf9050f453c113d8694e4e287da29f6988aa7e4
htpweb <aarboledaadv2@gmail.com>
2026-09-25 10:45:46 -0500

    Add HeiGIT optimization endpoint

--- supabase/functions/_shared/ors.ts
+++ supabase/functions/_shared/ors.ts
@@ -1,2 +1,5 @@
 export const ORS_DIRECTIONS_DRIVING_URL =
   "https://api.heigit.org/openrouteservice/v2/directions/driving-car";
+
+export const ORS_OPTIMIZATION_URL =
+  "https://api.heigit.org/vroom/v0";
