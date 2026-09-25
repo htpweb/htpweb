673ba2240e7c33053663855c179caeab21fd4419
htpweb <aarboledaadv2@gmail.com>
2026-09-25 10:29:49 -0500

    Keep ORS hotfix foundation minimal

--- supabase/functions/_shared/ors.ts
+++ supabase/functions/_shared/ors.ts
@@ -1,8 +1,2 @@
 export const ORS_DIRECTIONS_DRIVING_URL =
   "https://api.heigit.org/openrouteservice/v2/directions/driving-car";
-
-export const ORS_MATRIX_DRIVING_URL =
-  "https://api.heigit.org/openrouteservice/v2/matrix/driving-car";
-
-export const ORS_OPTIMIZATION_URL =
-  "https://api.heigit.org/vroom/v0";
